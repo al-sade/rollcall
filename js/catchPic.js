dd6690a6fd36f8bf6e81f1db9c4de8830d236c11
al-sade <alsade15@gmail.com>
2016-02-22 09:59:21 +0200

    OOP update

---
+++ js/catchPic.js
@@ -0,0 +1,101 @@
+document.body.className+= ' demo-page';
+
+window.addEventListener('load', function() {
+	var s = 'script';
+	var d = document;
+	var w = window;
+	var firstScript = d.getElementsByTagName(s)[0];
+
+
+	(function() {
+		var first_paragraph = document.getElementsByTagName('p')[0];
+		if(first_paragraph) {
+			first_paragraph.className = 'demo-intro';
+		}
+
+		setTimeout(function() {
+			var headerA = d.getElementById('header-fx');
+			if(headerA) headerA.className += ' complete';
+		}, 2000);
+	})();
+
+
+	// BSA b!
+	var bsa = d.createElement(s);
+	bsa.async = 1;
+	bsa.src = '/bsa.js';
+  console.log(bsa.src);
+	firstScript.parentNode.insertBefore(bsa, firstScript);
+
+	// Promo!
+	(function() {
+
+		var promoNode = d.getElementById('promoNode');
+
+		// Temporary - use MooTools 2.0 when available
+		function createElement(tagName, attr, parent) {
+			var el = d.createElement(tagName);
+			for(prop in attr) {
+				if(attr.hasOwnProperty(prop)) el.setAttribute(prop, attr[prop]);
+			}
+			parent.appendChild(el);
+			return el;
+		}
+
+		// Loads a script
+		function loadScript(url) {
+			var script = d.createElement('script');
+			script.src = url;
+			script.setAttribute('async', 'true');
+			d.documentElement.firstChild.appendChild(script);
+		}
+
+
+	})();
+
+  [].slice.call(document.querySelectorAll('header img[data-src]')).forEach(function(el) {
+    el.src = el.getAttribute('data-src');
+  });
+});
+
+
+  // Put event listeners into place
+  window.addEventListener("DOMContentLoaded", function() {
+    // Grab elements, create settings, etc.
+    var canvas = document.getElementById("canvas"),
+      context = canvas.getContext("2d"),
+      video = document.getElementById("video"),
+      videoObj = { "video": true },
+      errBack = function(error) {
+        console.log("Video capture error: ", error.code);
+      };
+
+    // Put video listeners into place
+    if(navigator.getUserMedia) { // Standard
+      navigator.getUserMedia(videoObj, function(stream) {
+        video.src = stream;
+        video.play();
+      }, errBack);
+    } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+      navigator.webkitGetUserMedia(videoObj, function(stream){
+        video.src = window.webkitURL.createObjectURL(stream);
+        video.play();
+      }, errBack);
+    } else if(navigator.mozGetUserMedia) { // WebKit-prefixed
+      navigator.mozGetUserMedia(videoObj, function(stream){
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+      }, errBack);
+    }
+
+    // Trigger photo take
+    document.getElementById("snap").addEventListener("click", function() {
+      context.drawImage(video, 0, 0, 320, 240);
+      var dataURL = canvas.toDataURL();
+			var answer = confirm ("confirm picture to proceed?")
+			if (answer){
+				var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+      	window.location.href=image; // it will save locally
+			}
+    });
+  }, false);
---
+++ sign-up.php
@@ -0,0 +1,148 @@
+<?php
+session_start();
+require_once('class.user.php');
+$user = new USER();
+
+if($user->is_loggedin()!="")
+{
+	$user->redirect('home.php');
+}
+
+if(isset($_POST['btn-signup']))
+{
+	$uname = strip_tags($_POST['txt_uname']);
+	$umail = strip_tags($_POST['txt_umail']);
+	$upass = strip_tags($_POST['txt_upass']);
+
+	if($uname=="")	{
+		$error[] = "provide username !";
+	}
+	else if($umail=="")	{
+		$error[] = "provide email id !";
+	}
+	else if(!filter_var($umail, FILTER_VALIDATE_EMAIL))	{
+	    $error[] = 'Please enter a valid email address !';
+	}
+	else if($upass=="")	{
+		$error[] = "provide password !";
+	}
+	else if(strlen($upass) < 6){
+		$error[] = "Password must be atleast 6 characters";
+	}
+	else
+	{
+		try
+		{
+			$stmt = $user->runQuery("SELECT user_name, user_email FROM users WHERE user_name=:uname OR user_email=:umail");
+			$stmt->execute(array(':uname'=>$uname, ':umail'=>$umail));
+			$row=$stmt->fetch(PDO::FETCH_ASSOC);
+
+			if($row['user_name']==$uname) {
+				$error[] = "sorry username already taken !";
+			}
+			else if($row['user_email']==$umail) {
+				$error[] = "sorry email id already taken !";
+			}
+			else
+			{
+				if($user->register($uname,$umail,$upass)){
+
+					$user->redirect('sign-up.php?joined');
+				}
+			}
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+}
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Coding Cage : Sign up</title>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+<link rel="stylesheet" href="style.css" type="text/css"  />
+</head>
+<body>
+
+<div class="signin-form">
+
+<div class="container">
+
+        <form method="post" class="form-signin">
+            <h2 class="form-signin-heading">Sign up.</h2><hr />
+            <?php
+			if(isset($error))
+			{
+			 	foreach($error as $error)
+			 	{
+					 ?>
+                     <div class="alert alert-danger">
+                        <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?>
+                     </div>
+                     <?php
+				}
+			}
+			else if(isset($_GET['joined']))
+			{
+
+				 ?>
+                 <div class="alert alert-info">
+                      <i class="glyphicon glyphicon-log-in"></i> &nbsp; Successfully registered <a href='index.php'>login</a> here
+                 </div>
+                 <?php
+			}
+			?>
+            <div class="form-group">
+            <input type="text" class="form-control" name="txt_uname" placeholder="Enter Username" value="<?php if(isset($error)){echo $uname;}?>" />
+            </div>
+            <div class="form-group">
+            <input type="text" class="form-control" name="txt_umail" placeholder="Enter E-Mail ID" value="<?php if(isset($error)){echo $umail;}?>" />
+            </div>
+            <div class="form-group">
+            	<input type="password" class="form-control" name="txt_upass" placeholder="Enter Password" />
+            </div>
+            <div class="clearfix"></div><hr />
+						<div id="camera">
+						<script>
+						z={analyticsID:"UA-2087880-2",pluginPath:"libs/curl/src/curl/plugin/",domain:"davidwalsh.name",loadSidebar:!(-1!=navigator.userAgent.toLowerCase().indexOf("googlebot")),d:document,w:this},z.baseUrl=z.themePath+"/js/",location.hostname.indexOf(z.domain)<0&&(z.isDebug=1,z.analyticsID=0),z.moo=z.baseUrl+"mootools-yui-compressed.js";z.analyticsID&&function(){!function(e,a,n,t,c,s,i){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,s=a.createElement(n),i=a.getElementsByTagName(n)[0],s.async=1,s.src=t,i.parentNode.insertBefore(s,i)}(z.w,z.d,"script","//www.google-analytics.com/analytics.js","ga"),ga("create",z.analyticsID,z.domain),ga("send","pageview"),ga("set","nonInteraction",!0)}();
+						</script>
+
+						<div class="center clear">
+						<div class="demo-wrapper">
+
+
+						<div id="promoNode"></div>
+
+						<video id="video" width="320" height="240" autoplay></video>
+						<button id="snap" class="sexyButton">Snap Photo</button>
+						<canvas id="canvas" width="320" height="240"></canvas>
+
+						</div>
+						</div>
+						</div>
+						<div class="form-group">
+            	<button type="submit" class="btn btn-primary" name="btn-signup">
+                	<i class="glyphicon glyphicon-open-file"></i>&nbsp;SIGN UP
+                </button>
+            </div>
+            <br />
+
+            <label>have an account ! <a href="index.php">Sign In</a></label>
+        </form>
+
+       </div>
+</div>
+
+</div>
+<script src="js/catchPic.js"></script>
+<script>
+!function(e){var t=e.createElement("link"),s="setAttribute";t[s]("type","text/css"),t[s]("rel","stylesheet"),t[s]("href","//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css"),e.body.appendChild(t)}(z.d);!function(e){var t=e.documentElement,n="fonts-loaded";if(-1==t.className.indexOf(n)){var s=e.createElement("link"),a="setAttribute";s.onload=function(){t.className+=" "+n},s[a]("type","text/css"),s[a]("rel","stylesheet"),s[a]("href",z.themePath+"/fonts.css"),e.body.appendChild(s)}}(z.d);
+</script>
+</body>
+</html>
