ca669ed987aabc8113bb77dff4e900ac4b02545b
al-sade <alsade15@gmail.com>
2016-03-20 22:33:19 +0200

    pre uriel's

--- save.php
+++ save.php
@@ -1,6 +1,9 @@
 <?php
-//error_reporting(E_ALL);
-//var_dump($_SERVER);
+require_once("session.php");
+
+require_once("class.user.php");
+$auth_user = new USER();
+
 $post_data = $_POST['pngUrl'];
 $user_id = $_POST['id'];
 
@@ -9,12 +12,15 @@ list($type, $post_data) = explode(';', $post_data);
 list(, $post_data)      = explode(',', $post_data);
 $post_data = base64_decode($post_data);
 
-file_put_contents('images/users/'.$user_id.'.png', $post_data);
+$path = "images/users/'.$user_id.'.png";
+file_put_contents($path, $post_data);
 }
+
+$auth_user->kairosEnroll('images/users/212121.png',$user_id);
 
 //This logic can be used to save base64_encode vevrsion of the image!
 // if (!empty($post_data)) {
-//     $dir = '/var/www/rollcall/images/users';
+//     // $dir = '/var/www/rollcall/images/users';
 //     $file = $user_id;
 //     $filename = dirname(__FILE__)."/images/users/".$file.'.txt	';
 //     $handle = fopen($filename, "w");
