f74baf9b50b3db440757759df19046eb62cbe37d
al-sade <alsade15@gmail.com>
2016-04-01 19:46:34 +0300

    blank pages and env sample

---
+++ env-sample.php
@@ -0,0 +1,6 @@
+<?php
+ define('HOST', 'db_host');
+ define('USER', 'db_user');
+ define('PASS', 'db_pass');
+ define('DB', 'db_name');
+?>
---
+++ notifications.php
@@ -0,0 +1,51 @@
+<?php
+
+	require_once("session.php");
+
+	require_once("class.user.php");
+
+	require 'vendor/autoload.php';
+	$auth_user = new USER();
+
+
+	$user_id = $_SESSION['user_session'];
+
+	$stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
+	$stmt->execute(array(":user_id"=>$user_id));
+
+	$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="vendor/twbs/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+<script type="text/javascript" src="components/jquery/jquery.min.js"></script>
+<link rel="stylesheet" href="style.css" type="text/css"  />
+<title>welcome - <?php print($userRow['email']); ?></title>
+</head>
+
+<body>
+
+<?php require_once('header.php');?>
+
+
+    <div class="clearfix"></div>
+
+
+<div class="container-fluid" style="margin-top:80px;">
+
+    <div class="container">
+
+    	<!-- <label class="h5">welcome : <?php print($userRow['first_name'].$userRow['last_name']); ?></label> -->
+
+    </div>
+
+</div>
+
+<script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
