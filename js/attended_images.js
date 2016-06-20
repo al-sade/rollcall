5d2ffc4384117ec994b42423ad09d27ced0bd61d
al.sade <al.s@omgstudios.com>
2016-06-20 18:50:08 +0300

    begin of appeal images of attended users

--- js/attended_images.js
+++ js/attended_images.js
@@ -1,12 +1,18 @@
-window.onload=function() {
-  document.getElementById("cause-dd").onchange=function() {
-
-  }
-}
 $(document).ready(function(){
+
   var table = '<div id="added" >';
-  table += "<p>Hello Worlddd</p></div>";
+  for (var i = 0; i < 10; i++) {
+    table += '<div id="' + i + '" class="appeal-img" style="background:url(https://s-media-cache-ak0.pinimg.com/avatars/al3069_1399274732_140.jpg);"></div>'
+  }
   $( "#cause-dd" ).change(function() {
-    $(table).insertAfter( "#cause-dd" );
+    if($(this).find('option:selected').val() == 1){
+      if(!$(this).find("#added").length){
+        console.log("ll");
+        table += "</div><br>";
+        $(table).insertAfter( "#cause-dd" );
+
+      }
+
+    }
   });
 });
